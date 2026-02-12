6a3461477edec523fe1e4b9017602fbb77c72c1c
unknown <mokshguptakatni@gmail.com>
2026-02-12 11:40:25 +0530

    New file

--- Binary-Search/search_rotated_array.py
+++ Binary-Search/search_rotated_array.py
@@ -1,12 +1,12 @@
 """
-Problem: Find Minimum in Rotated Sorted Array
+Problem: Search in Rotated Sorted Array
 Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/submissions/1913900745/
 Difficulty: Medium
 Topic: Array, Binary Search
-Date: 09-02-2026
+Date: 12-02-2026
 
 Approach:
-Find the middle element of the array and compare it with the rightmost element. If the middle element is less than or equal to the rightmost element, it means the minimum value is in the left half of the array, so we move the right pointer to mid. Otherwise, the minimum value is in the right half of the array, so we move the left pointer to mid + 1. Continue this process until left and right pointers converge, at which point left will be pointing to the minimum element.
+Find the middle element of the array and compare it with the target value. If they are equal, return the index. If the left half of the array is sorted (i.e., nums[low] <= nums[mid]), check if the target value is within the range of the left half. If it is, move the high pointer to mid - 1; otherwise, move the low pointer to mid + 1. If the right half of the array is sorted, check if the target value is within the range of the right half. If it is, move the low pointer to mid + 1; otherwise, move the high pointer to mid - 1. Continue this process until the target value is found or the search space is empty.
 
 Time Complexity: O(log n)
 Space Complexity: O(1)
@@ -14,15 +14,23 @@ Space Complexity: O(1)
 
 from typing import List
 
-
 class Solution:
-    def findMin(self, nums: List[int]) -> int:
-        left = 0
-        right = len(nums) - 1
-        while left < right:
-            mid = (left + right) // 2
-            if nums[mid] <= nums[right]:
-                right = mid
+    def search(self, nums: List[int], target: int) -> int:
+        if (len(nums) == 0):
+            return -1
+        
+        low = 0
+        high = len(nums)-1
+        while (low <= high):
+            mid = low+ ((high-low)//2)
+            if target == nums[mid]:
+                return mid
+            elif nums[low]<=nums[mid]:
+                if nums[low]<=target and target < nums[mid]:
+                    high=mid-1
+                else: low=mid+1
             else:
-                left = mid + 1
-        return nums[left]
+                if nums[mid] <= target and target <= nums[high]:
+                    low=mid+1
+                else: high=mid-1
+        return -1
