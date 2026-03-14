5575b38b34468e430adfad066a34d1c5b12e3977
unknown <mokshguptakatni@gmail.com>
2026-03-15 00:00:04 +0530

    Greedy

--- Greedy/candy.py
+++ Greedy/candy.py
@@ -2,7 +2,7 @@
 Problem: Candy
 Link: https://leetcode.com/problems/candy/submissions/1946348327/
 Difficulty: Hard
-Topic: Greedyy
+Topic: Greedy
 Date: 12-03-2026
 
 Approach: We can use a greedy approach to solve this problem. We will iterate through the ratings and assign candies to each child based on their ratings. We will ensure that each child has at least one candy and that children with higher ratings than their neighbors receive more candies. We will first iterate from left to right and assign candies based on the ratings compared to the previous child. Then, we will iterate from right to left and adjust the candies based on the ratings compared to the next child. Finally, we will sum up the total number of candies assigned.
